eb099f201352628778c9e43861a1cdd859ec9768
francis-xavier-inyangat <xavierfranzings@gmail.com>
2022-10-03 13:03:16 +0300

    modified

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require('express')
 
 
-const router = express.Router();
+const router = express.Router()
 
 
 
 
 router.get('/', (req, res) => {
-  
+
   res.render('homepage');
 });
 
